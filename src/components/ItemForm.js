8cca7d18d1701273aa8a7f0e35f27658ccb73ab1
Mohammed Zubair <mohammedzubair10@yahoo.com>
2022-02-07 14:43:12 -0500

    commit tree 13ac7f3e4cd532fa3fb8dfa85631b85699ccce92

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,17 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { v4 as uuid } from "uuid";
 
-function ItemForm(props) {
+function ItemForm({ addItem }) {
+  // There's a better way to do this by defining one state variable for both inputs  
+  const [name, setName] = useState('');
+  const handleNameChange = (e) => {
+    setName(e.target.value)
+  }
+
+  const [category, setCategory] = useState('Produce');
+  const handleCategoryChange = (e) => {
+    setCategory(e.target.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newItem = {
+      id: uuid(),
+      name,
+      category
+    };
+    addItem(newItem);
+  }
+
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit={handleSubmit}>
       <label>
         Name:
-        <input type="text" name="name" />
+        <input type="text" name="name" value={name} onChange={handleNameChange} />
       </label>
 
       <label>
         Category:
-        <select name="category">
+        <select name="category" onChange={handleCategoryChange} value={category}>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -3,23 +3,34 @@ import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
 
-function ShoppingList({ items }) {
+function ShoppingList({ items, addItem }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [searchValue, setSearchValue] = React.useState('');
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
+  }
+
+  const handleSearchValueChange = (e) => {
+    setSearchValue(e.target.value);
   }
 
   const itemsToDisplay = items.filter((item) => {
     if (selectedCategory === "All") return true;
 
     return item.category === selectedCategory;
+  }).filter(item => {
+    // 'Hello, how are you?'.search('how') // 7
+    // 'Hello, how are you?'.search('blahblah') // -1
+    if (item.name.search(searchValue) > -1) {
+      return true
+    }
   });
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <ItemForm addItem={addItem} />
+      <Filter onCategoryChange={handleCategoryChange} onSearchValueChange={handleSearchValueChange} searchValue={searchValue} />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
